0365a1f2ded7f5bad86d996e635133c823e4caa3
R Berry <rayberry@bluelightcard.co.uk>
2023-08-23 06:36:00 +0100

    Initial Commit

---
+++ MPDemo/MPDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MPDemoApp.swift
+//  MPDemo
+//
+//  Created by Ray Berry on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MPDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
